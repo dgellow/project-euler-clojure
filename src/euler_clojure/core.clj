b3d3dba74f4d1034b60e26958f07800c2ecb2a88
Sam E <georgesleyetidoux@gmail.com>
2013-05-29 10:25:18 +0200

    Problems 1 (multiples) and 2 (fibonacci)

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -1,6 +1,24 @@
 (ns euler-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; problem 1
+(defn multiples
+  "Returns all multiples of given numbers within (range 1 limit)"
+  [limit & numbers]
+  (filter
+   (fn [x] (reduce #(or %1 %2) (map (fn [n] (= (rem x n) 0)) numbers)))
+   (range 1 limit)))
+
+(defn sum-of-multiples
+  "Returns the sum of all the multiples of a given number"
+  [limit & numbers]
+  (reduce + (multiples limit numbers)))
+
+
+(defn fib-seq
+  "Generates a lazy sequence of all Fibonacci numbers."
+  [a b]
+  (cons a (lazy-seq (lazy-seq (fib-seq b (+' a b))))))
+
+(defn sum-of-fib
+  [max-value]
+  (reduce + (filter even? (take-while #(< % max-value) (fib-seq 1 1)))))
--- test/euler_clojure/core_test.clj
+++ test/euler_clojure/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [euler-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sumsofsmultiples-test
+  (testing "sum of multiple of 3 and 5 below 1000"
+    (is (== (sumofmultiple-test 1000 3 5) 233168))))
+
+(deftest sum-of-fib-test
+  (testing "sum of even fibonacci's numbers below 4mio"
+    (is (== (sum-of-fib 4000000) 4613732))))
